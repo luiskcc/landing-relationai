2189a3c09adee59bea35bb52d42d66329e37f5a1
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2023-04-03 15:56:49 +0100

    navbar footer sections

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 
-const typingText = "Relation AI is a simple sales marketing tool that helps you interact with customers just like this... |";
+const typingText = "Hi, I'm Relation AI, your new Sales Development Representative. My main role is to help generate and qualify leads for your sales team. With my assistance, you can streamline your sales process and increase revenue."
 const typingContainer = document.querySelector(".typing-container");
 const typingElement = document.querySelector("#typing-text");
 
 function typeText() {
-    typingElement.textContent = "";
-    let i = 0;
-    const typingInterval = setInterval(function() {
-        typingElement.textContent += typingText.charAt(i);
-        i++;
-        if (i > typingText.length) {
-            clearInterval(typingInterval);
-            setTimeout(bindText, 1000);
-        }
-    }, 100);
+  typingElement.textContent = "";
+  let i = 0;
+  const typingInterval = setInterval(function() {
+    typingElement.textContent += typingText.charAt(i);
+    i++;
+    if (i > typingText.length) {
+      clearInterval(typingInterval);
+      setTimeout(bindText, 1000);
+    }
+  }, 100);
 }
 
 function bindText() {
-    typingElement.textContent = typingText.replace("|", "");
-    typingElement.style.borderRight = "none";
+  typingElement.style.borderRight = "none";
 }
 
 typeText();
