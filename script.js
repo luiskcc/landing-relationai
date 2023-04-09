e019e126a0c51ccb6c916f7bd90f96f65211e283
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2023-04-09 10:35:51 +0100

    changes in JS typed-content increased pace of text

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const typingText = "Hi, I'm Relation AI, your new Sales Development Representative. My main role is to help generate and qualify leads for your sales team. With my assistance, you can streamline your sales process and increase revenue.";
-const typingContainer = document.querySelector(".typing-container");
-const typingElement = document.querySelector("#typing-text");
 
-function typeText() {
-    typingElement.textContent = "";
+
+const outputDiv = document.getElementById('typed-content')
+
+const txt = "Hi, I'm Relation AI, your new Sales Development Representative. My main role is to help generate and qualify leads for your sales team. With my assistance, you can streamline your sales process and increase revenue.";
+
+function typeText(){
+
     let i = 0;
-    const typingInterval = setInterval(function() {
-        typingElement.textContent += typingText.charAt(i);
-        i++;
-        if (i > typingText.length) {
-            clearInterval(typingInterval);
-            setTimeout(bindText, 1000);   
-        }
-    }, 100);
-    }
 
-    function bindText() {
-    typingElement.style.borderRight = "none";
-    }
+
+    setInterval(() => {
+
+        outputDiv.innerHTML += txt.charAt(i);
+        i++;
+    }, 50)
+
+}
 
 typeText();
 
