ef31a4e1b2bae5ba38e5f855bf18fb0c52842e54
“luiskcc” <114909465+luiskcc@users.noreply.github.com>
2023-04-10 17:18:04 +0100

    removed links

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 function setData()
-        {    
-        const txt = `
-        Hi,  <np>
-        <np>        
-
-        I'm Relation AI your new Sales Development Representative. <np>
-        My main role is to help generate and qualify leads for your sales team. <np>
-        With my assistance, you can streamline your sales process and increase revenue.<np>
-        #codeCandy Studio`;
-        const outputDiv = document.getElementById('typed-content');
-        return [txt, outputDiv]
-    };
+{    
+const txt = `
+Hi,<np>
+<np>
+I'm Relation AI your new Sales Development Representative. <np>
+My main role is to help generate and qualify leads for your sales team. <np>
+With my assistance, you can streamline your sales process and increase revenue.<np>
+#codeCandy Studio`;
+const outputDiv = document.getElementById('typed-content');
+return [txt, outputDiv]
+};
 const res =  setData();
 const txt = res[0];
 const outputDiv = res[1];
 const splittedTxt = txt.split('<np>');
 splittedTxt.forEach((item, index) => {
-  const p = document.createElement('p');
-  outputDiv.appendChild(p);
-  });
+const p = document.createElement('p');
+outputDiv.appendChild(p);
+});
 const allParas = outputDiv.querySelectorAll('p');
 let i = 0;
 let currentPara = 0;
